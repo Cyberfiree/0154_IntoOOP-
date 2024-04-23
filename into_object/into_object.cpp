3dee9d65139b90fb4f6cfb65fce400bd774da829
Cyberfire <buayagoreng99@gmail.com>
2024-04-23 11:17:19 +0800

    menampilkan informasi mahasiswa

--- into_object/into_object.cpp
+++ into_object/into_object.cpp
@@ -44,4 +44,5 @@ int main() //membuat fungsi utama
     mhs.nama = "Budi"; //set nama mahasiswa
     mhs.umur = 20; //set umur mahasiswa
     mhs.jurusan = "Teknik Informatika"; //set jurusan mahasiswa
+    mhs.output(); //tampilkan informasi mahasiswa
 }
